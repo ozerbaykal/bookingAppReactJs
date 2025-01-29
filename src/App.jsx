1f6d487753a4bad0c8643fca847fb1d7676560a8
ozerbaykal <baykalozer87@gmail.com>
2025-01-29 04:59:11 +0300

    feat: Implemented page structure using DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import About from "./pages/About";
+import Blogs from "./pages/Blogs";
+import BlogsDetails from "./pages/BlogsDetails";
+import PlacesRoute from "./pages/PlacesRoute";
+import Nopage from "./pages/Nopage";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div className="text-3xl">App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/blogs/:id" element={<BlogsDetails />} />
+            <Route path="/places" element={<PlacesRoute />} />
+            <Route path="*" element={<Nopage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="fixed top-0 right-0 w-full bg-white text-black shadow-md">
+        <div className="container">
+          <div className="flex bg-gradient-to-r from-primary ">
+            <p>20% off next booking</p>
+            <p>Mobile No.4444444</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
