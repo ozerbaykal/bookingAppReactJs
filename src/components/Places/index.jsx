8dbac224947bfb0f2f56a2f9eaafa151511e355e
ozerbaykal <baykalozer87@gmail.com>
2025-01-30 01:40:16 +0300

    feat: add PlacesData on constans

---
+++ src/components/Places/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PlaceCard from "./PlaceCard";
+
+const Places = () => {
+  return (
+    <>
+      <div className="dark:bg-gray-900 dark:text-white bg-gray-50 py-10">
+        <section data-aos="fade-up" className="container ">
+          <h1 className=" my-8 border-l-8 border-primary/50 py-2 pl-2 text-3xl font-bold">
+            Best Places to Visit
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {PlacesData.map((item, index) => (
+              <PlaceCard
+                handleOrderPopup={handleOrderPopup}
+                key={index}
+                {...item}
+              />
+            ))}
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Places;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,12 @@
+import Img1 from "../assets/images/image2.avif"
+import Img2 from "../assets/images/image3.webp"
+import Img3 from "../assets/images/lasvegas.jpg"
+import Img4 from "../assets/images/losangles"
+import Img5 from "../assets/images/sdneey.avif"
+import Img6 from "../assets/images/tacmahal.jpg"
+import Img7 from "../assets/images/underwater.webp"
+
+
 export const BarLinks = [
     {
         name: "Home",
@@ -29,4 +38,56 @@ export const DropdownLinks = [
         name: "Location",
         link: "/#location",
     },
+];
+
+export const PlacesData = [
+    {
+        img: Img3,
+        title: "Miami Beach",
+        location: "USA",
+        description: "Enjoy the stunning sandy beaches and vibrant nightlife of Miami.",
+        price: 6700,
+        type: "Relax & Adventure",
+    },
+    {
+        img: Img6,
+        title: "Taj Mahal",
+        location: "India",
+        description: "A UNESCO World Heritage Site, the Taj Mahal is an architectural masterpiece built by Mughal Emperor Shah Jahan.",
+        price: 6700,
+        type: "Cultural & Historical",
+    },
+    {
+        img: Img2,
+        title: "Great Barrier Reef",
+        location: "Australia",
+        description: "Explore the world’s largest coral reef system, home to diverse marine life and breathtaking underwater scenery.",
+        price: 6200,
+        type: "Nature & Adventure",
+    },
+    {
+        img: Img5,
+        title: "Sydney Opera House",
+        location: "Australia",
+        description: "A world-renowned architectural marvel, the Sydney Opera House is an iconic cultural and performing arts venue.",
+        price: 6700,
+        type: "Cultural & Relax",
+    },
+    {
+        img: Img4,
+        title: "Los Angeles",
+        location: "USA",
+        description: "The entertainment capital of the world, home to Hollywood, stunning beaches, and iconic landmarks.",
+        price: 6700,
+        type: "Urban & Entertainment",
+    },
+    {
+        img: Img3,
+        title: "Las Vegas",
+        location: "USA",
+        description: "The ultimate entertainment and nightlife destination, known for its world-class casinos, shows, and luxury resorts.",
+        price: 6200,
+        type: "Entertainment & Adventure",
+    },
+
 ];
