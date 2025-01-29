0ac24749cbb6bda019e69edeeeeb5d26244912b2
ozerbaykal <baykalozer87@gmail.com>
2025-01-29 12:38:05 +0300

    feat:add: Logo on Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+//import { LogoImg } from "../../assets/vocationLogo.png";
 
 const Navbar = () => {
   return (
     <>
       <div className="fixed top-0 right-0 w-full bg-white text-black shadow-md">
-        <div className="container">
-          <div className="flex bg-gradient-to-r from-primary ">
-            <p>20% off next booking</p>
-            <p>Mobile No.4444444</p>
+        <div className="bg-gradient-to-r from-primary to-secondary text-white">
+          <div className="container py-[2px] sm:block hidden ">
+            <div className="flex justify-between  ">
+              <p>20% off next booking</p>
+              <p>Mobile No.444444444</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="container py-3 sm:py-0">
+          <div className="flex justify-between items-center">
+            {/* logo */}
+            <div>
+              <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+                <img
+                  src="./src/assets/Vocation.png"
+                  alt="Gologo"
+                  className="h-16"
+                />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
